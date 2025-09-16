e560b0bf6614b98288ce0852708573f41f3cb1e9
vnosl <nolja30@naver.com>
2025-09-16 19:53:32 +0900

    씬 파일구분 임시 StartScene 제작

---
+++ Mori/Assets/Scenes/Junho/Start/Scripts/Startclick.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            SceneManager.LoadScene("Dialogue Scene");
+        }
+    }
+}
